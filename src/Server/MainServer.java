611d1934e928335abca391a86c08215b64bb39ff
NevLax <qwegornikitin@gmail.com>
2024-08-10 13:58:28 +0400

    The correct MainServer.java

--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -1,41 +1,22 @@
 package Server;
+import simpleNetwork.Transfer;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainServer {
 
-    private static Socket clientSocket;
-    private static ServerSocket server;
-    private static BufferedReader in;
-    private static BufferedWriter out;
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(6006);
+        Socket clientSocket = server.accept();
+        Transfer transfer = new Transfer(clientSocket.getInputStream(), clientSocket.getOutputStream());
 
-    public static void main(String[] args) {
-        try {
-            try {
-                server = new ServerSocket(6006);
-                System.out.println("Сервер запущен!");
-                clientSocket = server.accept();
-                try {
-                    in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                    out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
+        String word = transfer.Get();
+        transfer.Send("Привет, это Сервер! Подтверждаю, вы наисали : " + word + "\n");
 
-                    String word = in.readLine();
-                    System.out.println(word);
-                    out.write("Привет, это Сервер! Подтверждаю, вы наисали : " + word + "\n");
-                    out.flush();
 
-                } finally {
-                    clientSocket.close();
-                    in.close();
-                    out.close();
-                }
-            } finally {
-                System.out.println("Сервер закрыт!");
-                server.close();
-            }
-        }   catch (IOException e) {
-            System.err.println(e);
-        }
+        clientSocket.close();
+        transfer.close();
     }
 }
