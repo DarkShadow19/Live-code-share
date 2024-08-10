c91e9d7d2c3af3905117abde0c7cfbd8267718e8
NevLax <qwegornikitin@gmail.com>
2024-08-10 15:54:12 +0400

    send file like Lines

--- src/Client/MainClient.java
+++ src/Client/MainClient.java
@@ -5,10 +5,12 @@ import java.io.*;
 import java.net.Socket;
 
 public class MainClient {
-
+    static final String EOFWORDCODE = "Feni to la";
     public static void main(String[] args) throws IOException {
         Socket clientSocket = new Socket("localhost", 6006);
         Transfer transfer = new Transfer(clientSocket.getInputStream(), clientSocket.getOutputStream());
+
+        GetFileFromNetwork(transfer);
 
         System.out.println("Привет, напиши текст!");
         String word = transfer.GetTerminal();
@@ -18,5 +20,14 @@ public class MainClient {
 
         clientSocket.close();
         transfer.close();
-    } 
+    }
+
+    private static void GetFileFromNetwork(Transfer transfer) throws IOException {
+        String line;
+        do {
+            line = transfer.Get();
+            System.out.println(line);
+        } while (!line.equalsIgnoreCase(EOFWORDCODE));
+
+    }
 }
--- src/Server/MainServer.java
+++ src/Server/MainServer.java
@@ -6,11 +6,16 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class MainServer {
-
+    static final String EOFWORDCODE = "Feni to la";
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(6006);
         Socket clientSocket = server.accept();
         Transfer transfer = new Transfer(clientSocket.getInputStream(), clientSocket.getOutputStream());
+
+        BufferedReader fileRead = new BufferedReader(new FileReader("/home/nlx/source.txt"));
+
+        SendFile(transfer, fileRead);
+        fileRead.close();
 
         System.out.println("Сервер ожидает сообщение");
         String word = transfer.Get();
@@ -21,5 +26,14 @@ public class MainServer {
         clientSocket.close();
         transfer.close();
         server.close();
+    }
+
+    private static void SendFile(Transfer transfer, BufferedReader file) throws IOException {
+        while(file.ready()) {
+            String line = file.readLine();
+            System.out.println(line);
+            transfer.Send(line);
+        }
+        transfer.Send(EOFWORDCODE);
     }
 }
