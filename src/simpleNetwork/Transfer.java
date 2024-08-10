0e3425eca81efeb8b42149ef8eaf20ceb2bdb7ca
NevLax <qwegornikitin@gmail.com>
2024-08-10 12:14:36 +0400

    Description of the Transfer.java class for simple network interaction

---
+++ src/simpleNetwork/Transfer.java
@@ -0,0 +1,19 @@
+package simpleNetwork;
+
+public class Transfer {
+    /**
+     * @param message
+     */
+    public static void Send(String message) {
+        //TODO: write network sender method
+    }
+
+    /**
+     * //syda stream nado
+     * @return message
+     */
+    public static String Get() {
+        //TODO: write network
+        return "message";
+    }
+}
