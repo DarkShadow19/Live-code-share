64e5f139aba9f86fd8a6758e33c4bb8e97de4ed6
NevLax <qwegornikitin@gmail.com>
2024-08-10 11:40:51 +0400

    Description of the basic methods of working with files

---
+++ src/WorkWithFile/Reader.java
@@ -0,0 +1,8 @@
+package WorkWithFile;
+
+public class Reader {
+    public static String Read(String filename) {
+        //TODO: return file like String
+        return "typo file";
+    }
+}
---
+++ src/WorkWithFile/Writer.java
@@ -0,0 +1,7 @@
+package WorkWithFile;
+
+public class Writer {
+    public static void Write(String filename, String data) {
+        //TODO: write file recording
+    }
+}
